8735ed1ebd2b11940fb93816403e0aaf11478e72
TheBigRoomXXL <seb.lovergne@gmail.com>
2024-10-17 16:50:02 +0200

    misc: add form endpoint

--- main.py
+++ main.py
@@ -4,14 +4,24 @@ from starlette.responses import Response, StreamingResponse  # noqa: F401
 from starlette.routing import Route
 
 
-async def main(request: Request):
+async def request_stream(request: Request):
+    return StreamingResponse(request.stream(), media_type="text/plain")
+
+
+async def request_form(request: Request):
+    def stream_data_from_form():
+        for key, value in request.form():
+            print(f"{key}: {value}")
+            yield f"{key}: {value}\n"
+
     return StreamingResponse(request.stream(), media_type="text/plain")
 
 
 app = Starlette(
     debug=True,
     routes=[
-        Route("/", main, methods=["GET", "POST"]),
+        Route("/", request_stream, methods=["GET", "POST"]),
+        Route("/form", request_form, methods=["GET", "POST"]),
     ],
 )
 
--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
 curl -F "foo=bar" http://localhost:7000
+curl -F "foo=bar" http://localhost:7000/form
